704d4af52b316dd67e61cbadd3bf0e5304638204
Thalia Bravo <153110473+DevThalia@users.noreply.github.com>
2025-03-25 20:26:33 +0100

    S

--- solerBravoThaliaUF2/src/components/Lista.vue
+++ solerBravoThaliaUF2/src/components/Lista.vue
@@ -1,20 +1,35 @@
 <script setup>
 import { fetchAños } from '@/composables/fech';
 import { RouterLink } from 'vue-router';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
-const { años, cargando, error } = fetchAños();
+const años = ref([]);
+const cargando = ref(true);
+const error = ref(null);
+
+onMounted(async () => {
+    try {
+        const data = await fetchAños();
+        años.value = data;
+    } catch (err) {
+        error.value = err.message;
+    } finally {
+        cargando.value = false;
+    }
+});
 </script>
 
 <template>
     <div>
         <h1>Años</h1>
-        <ul>
-            <li v-for="año in años" :key="año.any">
-                <RouterLink :to="'/any/' + año.any">
+        <ul v-if="!cargando && !error">
+            <li v-for="año in años" :key="año.año">
+                <RouterLink :to="'/any/' + año.año">
                     {{ año.año }}
                 </RouterLink>
             </li>
         </ul>
+        <p v-if="cargando">Cargando...</p>
+        <p v-if="error">Error: {{ error }}</p>
     </div>
 </template>
